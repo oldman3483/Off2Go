c325cf2910ecb6d4b3244f8b51d826fcccbb42a0
Heidie1121 <heidielee1121@gmail.com>
2025-05-27 17:29:42 +0800

    First Version Upload

---
+++ Off2GoTests/Off2GoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Off2GoTests.swift
+//  Off2GoTests
+//
+//  Created by Heidie Lee on 2025/5/27.
+//
+
+import Testing
+@testable import Off2Go
+
+struct Off2GoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
